64f0fea707680de481740f55f965bf301d0fd434
Léa <lea.chaminadas@gmail.com>
2021-05-29 10:04:51 +0200

    WIP

--- app/javascript/src/application_v2/search.js
+++ app/javascript/src/application_v2/search.js
@@ -21,8 +21,21 @@ var vm = new Vue({
       fetch(reqURL)
         .then((resp) => resp.json())
         .then((response) => {
-          this.articleObj = response.results;
-          this.isResult = this.articleObj.length;
+          this.articleObj = response.results.reduce(function (
+            groups,
+            result
+          ) {
+            var firstType = result.type;
+            if (!groups[firstType]) {
+              groups[firstType] = [];
+            }
+            var entry = groups[firstType];
+            entry.push(result);
+
+            return groups;
+          },
+          {});
+          this.isResult = Object.keys(this.articleObj).length;
         })
         .catch(function (response) {
           console.log(response);
